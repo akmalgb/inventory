58414397670adbac4c4ca1b968687b097ce733b6
Akmal Gibran Baihaqi <akmalbaihaqigibran@gmail.com>
2022-08-15 23:43:25 +0700

    +customer, product, and user json api resource

---
+++ app/Http/Resources/CustomerResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->name,
+            'email' => $this->user->email,
+            'created_at' => $this->created_at,
+
+            // 'user_data' => new UserResource($this->user),
+        ];
+    }
+}
